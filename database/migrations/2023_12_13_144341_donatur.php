f86af708768d420bfa37c7aaa96263c332f2bc9c
mpdev <mnprasetya@gmail.com>
2023-12-14 05:17:00 +0700

    donatur migration

--- database/migrations/2023_12_13_144341_donatur.php
+++ database/migrations/2023_12_13_144341_donatur.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('donaturs', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('icon');
+            $table->string('name');
+            $table->string('donation');
+            $table->enum('status',['on','off'])->default('on');
             $table->timestamps();
         });
     }

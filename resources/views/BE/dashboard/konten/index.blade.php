b70a93461489852d41e39ad3e7dc155ecc9007aa
mnprasetya <mnprasetya@gmail.com>
2023-12-26 16:02:30 +0700

    add tooltip konten masuk

--- resources/views/BE/dashboard/konten/index.blade.php
+++ resources/views/BE/dashboard/konten/index.blade.php
@@ -11,7 +11,7 @@
 <div class="flex flex-col">
  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
   <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-    <div class="w-[1280px] h-[600px] overflow-auto">
+    <div class="w-[1280px] max-h-[600px] overflow-auto">
 
 
     <table class="min-w-[1400px] text-left text-sm font-light">
@@ -56,22 +56,47 @@
             @endforeach
         </select>
        </td>
-       {{-- ss1 --}}
-       <td class="w-[230px] p-2">
-        <img class="w-8 shadow" src="{{ asset($item->foto) }}" alt="{{ $item->name }} foto">
-       </td>
-       {{-- ss2 --}}
-       <td class="w-[230px] p-2">
-           <img class="w-8 shadow" src="{{ asset($item->foto) }}" alt="{{ $item->name }} foto">
-    </td>
+{{-- ss1 --}}
+<td class="w-[230px] p-2">
+    <div data-tooltip-target="tooltip-ss1-{{ $loop->index }}" class="relative">
+        <img loading="lazy" class="w-10 shadow" src="{{ asset($item->screenshoot1) }}" alt="screenshoot1">
+        <div id="tooltip-ss1-{{ $loop->index }}" role="tooltip" class="absolute w-96 z-[10000] invisible inline-block px-3 py-2 text-sm font-medium text-white bg-white-900 rounded-lg shadow-sm opacity-0 tooltip ">
+           <a href="{{ asset($item->screenshoot1) }}" target="_blank">
+            <img loading="lazy" class="w-full shadow" src="{{ asset($item->screenshoot1) }}" alt="screenshoot1">
+        </a> 
+        </div>
+    </div>
+</td>
+
+{{-- ss2 --}}
+<td class="w-[230px] p-2">
+    @if ($item->screenshoot2)
+        
+    <div data-tooltip-target="tooltip-ss2-{{ $loop->index }}" class="relative">
+        <img loading="lazy" class="w-10 shadow" src="{{ asset($item->screenshoot2) }}" alt="screenshoot2">
+        <div id="tooltip-ss2-{{ $loop->index }}" role="tooltip" class="absolute w-96 z-[10000] invisible inline-block px-3 py-2 text-sm font-medium text-white bg-white-900 rounded-lg shadow-sm opacity-0 tooltip ">
+            <a href="{{ asset($item->screenshoot1) }}" target="_blank">
+            <img loading="lazy" class="w-full shadow" src="{{ asset($item->screenshoot2) }}" alt="screenshoot2">
+            </a>
+        </div>
+    </div>
+    @else
+        -
+    @endif
+</td>
     {{-- link --}}
     <td class="w-[230px] p-2">
-        <span class="text-zinc-800 text-sm font-normal font-Poppins text-wrap">{{ $item->link }}</span>
+        <a href="{{  $item->link }}" target="_blank">
+            <span class="text-red-600 text-sm font-normal font-Poppins text-wrap">{{ Str::of($item->link)->limit(20) }}</span>
+        </a>
        </td>
-       {{-- detail --}}
-       <td class="w-[230px] p-2">
-        <span class="text-zinc-800 text-sm font-normal font-Poppins text-wrap">{{ $item->detail }}</span>
-       </td>
+        {{-- detail --}}
+        <td class="w-[260px] p-2">
+            <span data-tooltip-target="tooltip-no-arrow-{{ $loop->index }}" class="text-zinc-800 text-sm font-normal font-Poppins text-wrap">{{ Str::of($item->detail)->limit(45) }}</span>
+            <div id="tooltip-no-arrow-{{ $loop->index }}" role="tooltip" class="absolute z-[10000] invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip ">
+                {{ $item->detail }}
+            </div>
+        </td>
        {{-- email --}}
        <td class="w-[230px] p-2">
         <span class="text-zinc-800 text-sm font-normal font-Poppins text-wrap">{{ $item->email }}</span>
@@ -85,20 +110,11 @@
         <td class="w-[100px] p-2 px-6 flex justify-center sticky right-0  z-10 bg-neutral-100 ">
 
         <button onclick="editModal('{{ $item->id }}')">
-         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M3 17.4595V20.4995C3 20.7795 3.22 20.9995 3.5 20.9995H6.54C6.67 20.9995 6.8 20.9495 6.89 20.8495L17.81 9.93951L14.06 6.18951L3.15 17.0995C3.05 17.1995 3 17.3195 3 17.4595ZM20.71 7.03951C20.8027 6.947 20.8762 6.83711 20.9264 6.71614C20.9766 6.59517 21.0024 6.46548 21.0024 6.33451C21.0024 6.20355 20.9766 6.07386 20.9264 5.95289C20.8762 5.83192 20.8027 5.72203 20.71 5.62951L18.37 3.28951C18.2775 3.19681 18.1676 3.12326 18.0466 3.07308C17.9257 3.0229 17.796 2.99707 17.665 2.99707C17.534 2.99707 17.4043 3.0229 17.2834 3.07308C17.1624 3.12326 17.0525 3.19681 16.96 3.28951L15.13 5.11951L18.88 8.86951L20.71 7.03951Z" fill="#FF7366" />
-         </svg>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.99991 9.00026V7.41026C9.99991 6.52026 8.91991 6.07026 8.28991 6.70026L3.69991 11.2903C3.60721 11.3828 3.53366 11.4927 3.48348 11.6136C3.4333 11.7346 3.40747 11.8643 3.40747 11.9953C3.40747 12.1262 3.4333 12.2559 3.48348 12.3769C3.53366 12.4979 3.60721 12.6077 3.69991 12.7003L8.28991 17.2903C8.91991 17.9203 9.99991 17.4803 9.99991 16.5903V14.9003C14.9999 14.9003 18.4999 16.5003 20.9999 20.0003C19.9999 15.0003 16.9999 10.0003 9.99991 9.00026Z" fill="#FF7366"/>
+                </svg>
+                
         </button>
-        <form method="post" action="{{ route('dashboard.anggota-tim.delete', $item->id) }}">
-         @csrf
-         @method('DELETE')
-         <input type="hidden" value="{{ $item->id }}" name="id" hidden>
-         <button type="submit">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-           <path d="M7 21C6.45 21 5.97933 20.8043 5.588 20.413C5.19667 20.0217 5.00067 19.5507 5 19V6H4V4H9V3H15V4H20V6H19V19C19 19.55 18.8043 20.021 18.413 20.413C18.0217 20.805 17.5507 21.0007 17 21H7ZM9 17H11V8H9V17ZM13 17H15V8H13V17Z" fill="#FF7366" />
-          </svg>
-         </button>
-        </form>
        </td>
       </tr>
       @empty
@@ -113,134 +129,58 @@
 </div>
 @endsection
 
-@push('css')
-<style>
-    /* Custom scroll bar styling */
-::-webkit-scrollbar {
-    width: 2px; /* Set width to 417px */
-    height: 2px; /* Set height to 2px */
-}
 
-/* Track */
-::-webkit-scrollbar-track {
-    background: #f1f1f1; /* Color of the track */
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-    background: #f44336; /* Color of the scroll thumb, but you want red-400 */
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-    background: #b71c1c; /* Darker color when hovering */
-}
-
-</style>
-    
+@push('javascript')
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script>
+$(document).ready(function() {
+    // Mengatur event hover pada semua elemen span dengan data-tooltip-target
+    $('[data-tooltip-target]').hover(
+        function() {
+            const targetId = $(this).data('tooltip-target');
+            const tooltip = $('#' + targetId);
+            tooltip.removeClass('invisible');
+            tooltip.addClass('opacity-100');
+        },
+        function() {
+            const targetId = $(this).data('tooltip-target');
+            const tooltip = $('#' + targetId);
+            tooltip.removeClass('opacity-100');
+            tooltip.addClass('invisible');
+        }
+    );
+});
+</script>
 @endpush
 
- @push('javascript')
- <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
- <script>
-  // preview image uplaod
-  document.addEventListener("DOMContentLoaded", function() {
-   const bestMomentInput = document.getElementById('foto');
-   const bestMomentPreview = document.getElementById('preview_foto');
-   bestMomentInput.addEventListener('change', (event) => {
-    handleImageUpload(event, 'preview_foto2');
-   });
-   const modal = document.getElementById("modalKirim");
-   const modalKirimKonten = document.getElementById("modalKirimKonten");
-   const closeModalBtn = document.getElementById("closeModalBtn");
-   modalKirimKonten.addEventListener("click", function() {
-    toggleModal(true);
-    clear();
-    gambarTim.setAttribute("hidden", "true");
-    // $('#title').text("Tambah Anggota Tim");
-   });
-   closeModalBtn.addEventListener("click", function() {
-    toggleModal(false);
-    clear();
-   });
+@push('css')
+<style>
+ /* Custom scroll bar styling */
+ ::-webkit-scrollbar {
+  width: 2px;
+  /* Set width to 417px */
+  height: 2px;
+  /* Set height to 2px */
+ }
 
-   // Event listener saat pengguna mengetik di input
-   $('#modalKirim input[name="quote"]').on('input', function() {
-    let currentLength = $(this).val().length;
-    let maxLength = parseInt($(this).attr('maxlength'));
-    let remainingChars = maxLength - currentLength;
-    $('#charCount').text(remainingChars + ' karakter tersisa');
-    if (currentLength >= maxLength) {
-     $(this).addClass('bg-green-200');
-    } else {
-     $(this).removeClass('bg-green-200');
-    }
-   });
+ /* Track */
+ ::-webkit-scrollbar-track {
+  background: #f1f1f1;
+  /* Color of the track */
+ }
 
-  });
+ /* Handle */
+ ::-webkit-scrollbar-thumb {
+  background: #f44336;
+  /* Color of the scroll thumb, but you want red-400 */
+ }
 
-  function toggleModal(isOpen) {
-   const modal = document.getElementById("modalKirim");
-   const action = isOpen ? "add" : "remove";
-   modal.classList[action]("opacity-100", "translate-y-0");
-   if (!isOpen) {
-    modal.classList.add("opacity-0", "translate-y-4");
-    setTimeout(() => {
-     modal.classList.add("hidden");
-     modal.classList.remove("opacity-0", "translate-y-4");
-    }, 500);
-   } else {
-    modal.classList.remove("hidden");
-   }
-  }
+ /* Handle on hover */
+ ::-webkit-scrollbar-thumb:hover {
+  background: #b71c1c;
+  /* Darker color when hovering */
+ }
 
-  function clear() {
-   $('#title').text("Tambah Anggota Tim");
-   $('#modalKirim input[name="id"]').val("");
-   $('#modalKirim input[name="name"]').val("");
-   $('#modalKirim input[name="quote"]').val("");
-   $('#modalKirim select[name="team_id"]').val("");
-   $('#modalKirim input[name="foto"]').val("");
-   const gambarTim = document.getElementById("preview_foto");
-   gambarTim.setAttribute("hidden", "true"); // Menyembunyikan gambar saat modal ditutup
-  }
+</style> 
+@endpush
 
-  function editModal(teamId) {
-   $('#title').text("Edit Anggota Tim");
-   toggleModal(true);
-   $.ajax({
-    type: "GET"
-    , url: "{{ route('dashboard.anggota-tim.edit', '') }}/" + teamId
-    , dataType: "json"
-    , success: function(anggotaData) {
-     // console.info(anggotaData);
-     fillForm(anggotaData);
-    }
-    , error: function(xhr, status, error) {
-     console.error('Gagal memuat data tim:', status);
-    }
-   });
-  }
-
-  function fillForm(anggotaData) {
-   // Mengambil referensi modal
-   const modal = document.getElementById("modalKirim");
-   // Menghapus kelas 'hidden' dan menambahkan kelas untuk menampilkan modal
-   $(modal).removeClass("hidden").addClass("opacity-100 translate-y-0");
-   // Mengisi nilai formulir dengan data tim
-   $('#modalKirim input[name="id"]').val(anggotaData.id);
-   $('#modalKirim input[name="quote"]').val(anggotaData.quote);
-   $('#modalKirim input[name="name"]').val(anggotaData.name);
-   $('#modalKirim select[name="team_id"]').val(anggotaData.team_id);
-   // $('#modalKirim input[name="foto"]').val(anggotaData.foto);
-   // console.log("Data tim:", anggotaData);
-   // Mengambil referensi elemen gambar tim
-   const gambarTim = document.getElementById("preview_foto");
-   // Menetapkan sumber gambar sesuai dengan URL yang diterima dari data tim
-   gambarTim.src = anggotaData.foto;
-   gambarTim.removeAttribute("hidden");
-   // console.log(anggotaData);
-  }
-
- </script>
- @endpush
